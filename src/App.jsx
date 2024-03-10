ec8bea16c726cad58f3f3763624d9268977b47be
Samuel Roberto <samucada160@gmail.com>
2024-03-10 18:02:13 -0300

    iteraçao

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,34 @@ import { Sidebar } from './components/Sidebar'
 
 const posts = [
   {
-    id: 123,
+    id: 1,
     author: {
       avatarUrl: 'https://github.com/samuelrobrodrigues.png',
-      name: 'Samuel Robert',
+      name: 'Samuel Roberto',
       role: 'Web developer',
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋',},
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz do zero, um dos meus primeiros projetos solo. O nome do projeto é Bernadelli 🚀',},
       { type: 'link', content: 'samuelRobRodrigues/Projeto-Bernadelli'},
-    ]
+    ],
+    publishedAt: new Date('2024-02-22 20:02:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/marioxxf.png',
+      name: 'Mario Luciano',
+      role: 'Pleno @ Fiserv',
+    },
+    content: [
+      { type: 'paragraph', content: 'Boa noite 👋',},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto. É um projeto que fiz de uma plataforma de filmes. O nome do projeto é Gioiaflix🚀',},
+      { type: 'link', content: 'marioxxf/gioiaflix'},
+    ],
+    publishedAt: new Date('2024-03-09 16:48:00'),
   }
-]
+];
 export function App() {
 
   return (
@@ -33,8 +48,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+            )
+          })}
         </main>
       </div>
     </div>
